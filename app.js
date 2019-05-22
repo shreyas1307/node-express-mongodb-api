70f3dfb11d01bf46e58676f94dee2748bf025775
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-05-22 10:09:29 +0530

    Added rootdir routes and render html files

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+// Node Modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
+// Root Directory File
+const rootDir = require("./util/path");
+
+// Importing Route Files
 const apiDataRoutes = require("./routes/api-data");
+
 // Dotenv file validation
 require("dotenv");
+
 // Creating Express App, Enabling Body-parser for Inputs
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Importing routes
+//Routes
 
 app.use("/api-data", apiDataRoutes);
 
--- routes/api-data.js
+++ routes/api-data.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const route = express.Router();
+const path = require("path");
+const rootDir = require("../util/path");
 
 route.get("/", (req, res) => {
-  res.send("<h1> Connection Success </h1>");
+  res.sendFile(path.join(rootDir, "views", "index.html"));
 });
 
 route.post("/", (req, res) => {
+  console.log(req.body.users, req.body.age);
   res.send("<h1>Your data has been submitted!</h1>");
 });
 
