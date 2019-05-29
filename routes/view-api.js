6ed2898fa7ea2fd04a7405f89dbb6113eb76a53e
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-05-29 23:02:14 +0530

    Misc.

--- routes/view-api.js
+++ routes/view-api.js
@@ -6,8 +6,8 @@ const Project = require("../model/schema");
 
 route.get("/", (req, res) => {
   Project.find()
-    .sort({ index: -1 })
-    .then(projects => console.log(projects));
+    // .sort({ index: -1 })
+    .then(projects => res.json(projects));
 });
 
 module.exports = route;
